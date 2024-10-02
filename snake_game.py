5b93a934d7b79bc33b4b776520029d9b6bf6dc63
dnlaylo <laylomarga@gmail.com>
2024-10-02 18:10:14 +0800

    Tile class for x and y positioning

--- snake_game.py
+++ snake_game.py
@@ -7,3 +7,8 @@ TILE_SIZE = 25
 
 WINDOW_WIDTH = TILE_SIZE * COLS #25*25 = 625
 WINDOW_HEIGHT = TILE_SIZE * ROWS #25*25 = 625
+
+class Tile:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
